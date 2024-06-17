f31736c5d36e4951e6cc287828e97c6fc12cc81b
rezahusain <rezahusain99@gmail.com>
2024-06-17 13:14:00 -0500

    Added create and delete photo feature

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,8 +4,41 @@ class PhotosController < ApplicationController
     matching_photos = Photo.all
 
     @list_of_photos = matching_photos.order({:created_at => :desc})
-    
+
     render({:template => "photos_templates/index"})
   end
 
+  def show
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({:id => url_id})
+
+    @the_photo = matching_photos.at(0)
+
+    render({:template => "photos_templates/show"})
+  end
+
+  def delete
+    the_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({:id => the_id})
+
+    the_photo = matching_photos.first
+
+    the_photo.destroy
+
+    redirect_to("/photos")
+  end
+
+  def create
+    @the_photo = Photo.new
+    image_value = params.fetch("image")
+    caption_value = params.fetch("caption")
+    owner_value = params.fetch("owner")
+    @the_photo.image = image_value
+    @the_photo.caption = caption_value
+    @the_photo.owner_id = owner_value
+    @the_photo.save
+    redirect_to("/photos")
+  end
 end
