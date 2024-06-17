a2d3415ea765275bf1147a25b8f0e5edef5f5d21
rezahusain <rezahusain99@gmail.com>
2024-06-17 10:55:01 -0500

    Finished user details page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,20 @@ class UsersController < ApplicationController
 
   def index
     matching_users = User.all
+
     @list_of_users = matching_users.order({:username => :asc})
+
     render({:template=>"user_templates/index"})
   end
 
   def show
-    
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({:username => url_username})
+
+    @the_user = matching_usernames.first
+
+    render({:template => "user_templates/show"})
   end
 
 end
