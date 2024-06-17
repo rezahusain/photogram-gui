1bdbf216f96776bed7fc03caaf44ab104fac62b1
rezahusain <rezahusain99@gmail.com>
2024-06-17 11:18:48 -0500

    Finished photo index page

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,11 @@
+class PhotosController < ApplicationController
+
+  def index
+    matching_photos = Photo.all
+
+    @list_of_photos = matching_photos.order({:created_at => :desc})
+    
+    render({:template => "photos_templates/index"})
+  end
+
+end
